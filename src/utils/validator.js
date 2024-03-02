822bf6700d80494ab7b23d332993afb1c779574a
lcruz-2019072 <lcruz-2019072@kinal.edu.gt>
2024-03-02 01:29:55 -0600

    comenzando

---
+++ src/utils/validator.js
@@ -0,0 +1,17 @@
+//validar diferentes datos
+'use strict'
+
+import { hash, compare} from 'bcrypt'
+
+export const encrypt = (password)=>{
+    try {
+
+        return hash(password, 10)
+
+    } catch (error) {
+        console.error(error)
+        return error
+    }
+
+}
+
